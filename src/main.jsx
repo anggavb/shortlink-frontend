b798bd7832884d2c0c4d8f603263a31b95df970c
anggavb <anggavb8@gmail.com>
2026-06-18 02:52:44 +0700

    fix(main): correct import order for store and persistor

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router";
 import AppRouter from "./AppRouter.jsx";
 import AppToastContainer from "@/components/ui/AppToastContainer.jsx";
-import { persistor, store } from "@/redux/store";
+import store, { persistor } from "@/redux/store";
 import "./tailwind.css";
 
 createRoot(document.getElementById("root")).render(
